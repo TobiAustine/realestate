e146071e2fd52cc5dde4ae41fd515b8f264f4dd1
ThatDivDoctor <autineosiene@gmail.com>
2023-12-14 02:43:38 +0100

    DB connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express()
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
+const mongodb = process.env.MONGODB
+
+
+mongoose.connect(mongodb)
+    .then(() => {console.log('db connected');})
+    .catch((error) => {console.log(error);})
 app.listen(8000, () =>{
     console.log('Server running');
-}) 
+})  
